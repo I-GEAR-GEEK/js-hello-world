59abb0ed555d9bf116e74cb70d451eaef0ce2fad
namwan104370 <napatkon0069@gmail.com>
2022-08-27 08:08:44 +0000

    virus

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  return res.send('<h1>สวัสดีจากน้ำหวาน.</h1>')
+  return res.send('<h1>สวัสดีจากน้ำหวาน. นี่คือไวรัส</h1>')
 })
 
 const PORT = process.env.PORT || 3000
