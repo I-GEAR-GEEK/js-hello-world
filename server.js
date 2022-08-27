fa6d3a98c99a364aeb0d4b9aec692731b0ce45aa
DoNoDus <chirapon096@gmail.com>
2022-08-27 04:53:32 +0000

    หิวหว่ะะะ

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  return res.send('<h1>รักสาวๆเมืองพะเยามากๆนะครับ</h1>')
+  return res.send('<h1>หิวส้นตีนวัยวุ่นเมื่อไหร่จะโดนสักที</h1>')
 })
 
 const PORT = process.env.PORT || 3000
