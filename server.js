0ae39f0583bdb4277dc21eb595d669c5980f4456
DoNoDus <chirapon096@gmail.com>
2022-08-27 06:59:41 +0000

    final update

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  return res.send('<h1>หิวส้นตีนวัยวุ่นเมื่อไหร่จะโดนสักที</h1>')
+  return res.send('<h1>หิวส้นตีนวัยวุ่นเมื่อไหร่จะโดนสักที hi </h1>')
 })
 
 const PORT = process.env.PORT || 3000
