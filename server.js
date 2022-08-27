dea01cbb0e1562cdfc2eb3a96b1d5eec8534c1b8
namwan104370 <napatkon0069@gmail.com>
2022-08-27 07:18:39 +0000

    main now

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  return res.send('<h1>ฮัลโหล main</h1>')
+  return res.send('<h1>this is main now</h1>')
 })
 
 const PORT = process.env.PORT || 3000
