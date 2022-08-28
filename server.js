3da938e8fc88ca259a770bb590dc74c2ff2618b1
DoNoDus <chirapon096@gmail.com>
2022-08-28 07:47:08 +0000

    NutCommit

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  return res.send('<h1>main branch hello hello hello</h1>')
+  return res.send('<h1>4 king</h1>')
+})
+
+app.get('/donodus', (req, res) => {
+  return res.send('<h1>Hi my name is jiraphon</h1>')
 })
 
 const PORT = process.env.PORT || 3000
