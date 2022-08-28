92273843624879e932fa4446ac8c0ca34c7e4c11
maitongpooddd <Kwanrutai.2544@gmail.com>
2022-08-28 07:50:32 +0000

    update

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ app.get('/', (req, res) => {
   return res.send('<h1>น้องเนสคนสวยและน่ารัก!</h1>')
 })
 
+app.get('/nes', (req, res) => {
+  return res.send('<h1>สวยและน่ารักที่สุดเลย</h1>')
+})
+
+
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.info('Server is ready at', PORT)
