829a0f64d847593dcb40fd9ea474a1c4a02370af
DoNoDus <chirapon096@gmail.com>
2022-08-28 06:31:50 +0000

    Update text

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  return res.send('<h1>How to do that?</h1>')
+  return res.send('<h1>develop branch</h1>')
 })
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.info('Server is ready at', PORT)
 }) 
+
+
