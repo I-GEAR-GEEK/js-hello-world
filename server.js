67c4780a80de53a788ef02858588b07ffd122c5f
lxxjxrn <almond26930@gmail.com>
2022-08-28 08:15:28 +0000

    M

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
   return res.send('<h1>Hyunsuk</h1>')
 })
 app.get('/almond', (req, res) => {
-  return res.send('<h1>JAYM</h1>')
+  return res.send('<h1>JAYM LEE</h1>')
 })
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
