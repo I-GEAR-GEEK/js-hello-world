880817cfba9fb47d0e5793903f3350d5e59789d2
Rimuru <koonmilemkcc@gmail.com>
2022-08-28 07:44:47 +0000

    add mile

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  return res.send('<h1>main branch hello hello hello</h1>')
+  return res.send('<h1>main branch hi</h1>')
+})
+app.get('/mile', (req, res) => {
+  return res.send('<h1>สวัดีครับ</h1>')
 })
 
 const PORT = process.env.PORT || 3000
